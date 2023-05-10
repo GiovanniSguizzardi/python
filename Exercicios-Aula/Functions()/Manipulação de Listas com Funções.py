5f1904b09672cc8bd77321d8bea45bbfcac89c10
Gibas <125572342+GiovanniSguizzardi@users.noreply.github.com>
2023-05-10 11:55:48 -0300

    Add files via upload

---
+++ Exercicios-Aula/Functions()/Manipulação de Listas com Funções.py
@@ -0,0 +1,67 @@
+#Manipulação de Listas com Funções
+'''
+1) Função tamanho_lista() Feito
+2) Função criar_lista() Feito
+3) Função imprimir_lista() Feito
+4) Função imprimir_pares() Feito
+5) Função imprimir_impares() Feito
+6) Função que adiciona os pares em uma lista() Feito
+7) Função que adiciona os impares em uma lista() Feito
+8) Função busca um elemento na lista()
+'''
+
+def tamanho_lista():
+    print('[DEF] Tamanho Lista')
+    t = int(input('[?] Tamanho da lista: '))
+    return t
+
+def criar_lista(t):
+    print('\n[DEF] Criar lista')
+    lista = [] #cria lista vazia
+    i = 0 #variavel de controle
+    while i < t:
+        #lista[i] = int(input('Numero: '))
+        n = int(input('[?] Numero: '))
+        lista.append(n)
+        i+=1
+    return lista
+
+def imprimir_pares(lista):
+    print('\n[DEF] Pares')
+    lista_p = [] #cria lista vazia
+    for i in lista:
+        if i%2 == 0:
+            print(i)
+            lista_p.append(i)
+    return lista_p
+
+def imprimir_impares(lista):
+    print('\n[DEF] Impares')
+    lista_i = [] #cria lista vazia
+    for i in lista:
+        if i%2 != 0:
+            print(i)
+            lista_i.append(i)
+    return lista_i
+
+def imprimir_lista(lista):
+    print('\n[DEF] Imprimir Lista')
+    for i in lista:
+        print(f'[!] Elemento: {i}')
+    print(f'Lista: {lista}')
+
+def buscar_na_lista(lista):
+    print('\n[DEF] Buscar Elemento na Lista')
+    s = int(input('Qual slot deseja buscar: '))
+    lista.position
+
+def principal():
+    print('[DEF] Função Principal')
+    t = tamanho_lista()
+    lista = criar_lista(t)
+    pares = imprimir_pares(lista)
+    impares = imprimir_impares(lista)
+    imprimir = imprimir_lista(lista)
+
+#Programa Principal
+principal()
